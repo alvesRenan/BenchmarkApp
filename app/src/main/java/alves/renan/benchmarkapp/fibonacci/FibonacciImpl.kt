b44cd21517bbda275af432c868d6ac59cb454750
Filipe Fernandes <filipe.fernandes@crateus.ufc.br>
2020-10-26 15:26:14 -0300

    Fixed Fibonacci App
    Added reference in the AndroidManifest file
    Modified FibonacciImpl to return two firsts elements of sequence (1 and 1)

--- app/src/main/java/alves/renan/benchmarkapp/fibonacci/FibonacciImpl.kt
+++ app/src/main/java/alves/renan/benchmarkapp/fibonacci/FibonacciImpl.kt
@@ -3,7 +3,7 @@ package alves.renan.benchmarkapp.fibonacci
 class FibonacciImpl : Fibonacci {
 
     override fun compFibonacci(n: Int): Int {
-        return if (n < 2) {
+        return if (n <= 2) {
             1
         } else {
             compFibonacci(n - 2 ) + compFibonacci(n - 1)
